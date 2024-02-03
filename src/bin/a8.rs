6d0ecda88380c2eff5be732af622833dc969b0fd
Ore Akintobi <oreakintobi@gmail.com>
2024-02-03 14:06:33 +0100

    my first rust struct implementation

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,51 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum DrinksFlavor {
+    Vanilla,
+    Strawberry,
+    Chocolate,
+    Kiwi,
+}
+
+struct Drink {
+    flavor: DrinksFlavor,
+    fluid_ounce: f64,
+}
+
+fn print_drink_details(drink: Drink) -> () {
+    match drink.flavor {
+        DrinksFlavor::Chocolate => println!("The drink flavor is Chocolate"),
+        DrinksFlavor::Strawberry => println!("The drink flavor is Strawberry"),
+        DrinksFlavor::Vanilla => println!("The drink flavor is Vanilla"),
+        DrinksFlavor::Kiwi => println!("The drink flavor is Kiwi"),
+    }
+    println!("The drink weighs {:?} ounces", drink.fluid_ounce);
+}
+
+fn main() {
+    let drink1 = Drink {
+        flavor: DrinksFlavor::Vanilla,
+        fluid_ounce: 3.20,
+    };
+
+    let drink2 = Drink {
+        flavor: DrinksFlavor::Chocolate,
+        fluid_ounce: 3.20,
+    };
+
+    let drink3 = Drink {
+        flavor: DrinksFlavor::Strawberry,
+        fluid_ounce: 3.20,
+    };
+
+    let drink4 = Drink {
+        flavor: DrinksFlavor::Kiwi,
+        fluid_ounce: 3.20,
+    };
+
+    print_drink_details(drink1);
+    print_drink_details(drink2);
+    print_drink_details(drink3);
+    print_drink_details(drink4);
+}
