7fc85a867d0236bb6c3cc1416b1ae2af5ff575b3
Ore Akintobi <oreakintobi@gmail.com>
2024-02-03 12:50:10 +0100

    my second rust function (add two numbers)

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,11 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+    //Function to add 2 numbers
+    fn add(a: i32, b: i32) -> () {
+        println!("The sum of {:?} and {:?} is {:?}", a, b, a + b);
+    }
+
+    add(2, 3);
+}
