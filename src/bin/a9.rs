6071969064d4f519412d354774b1d65d321e1708
Ore Akintobi <oreakintobi@gmail.com>
2024-02-05 19:26:32 +0100

    my first rust tuple implementation

--- src/bin/a9.rs
+++ src/bin/a9.rs
@@ -9,4 +9,31 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+struct JustXAndY {
+    x: i32,
+    y: i32,
+}
+
+fn get_coords(coords: JustXAndY) -> (i32, i32) {
+    (coords.x, coords.y)
+}
+
+fn cartesian_coordinates(coord: JustXAndY, compare_number: i32) -> () {
+    let (x, y) = get_coords(coord);
+
+    print!("x is {:?}, ", x);
+
+    if y < compare_number {
+        println!("y is less than {:?}", compare_number)
+    } else if y > compare_number {
+        println!("y is greater than {:?}", compare_number)
+    } else {
+        println!("y is equal to {:?}", compare_number)
+    }
+}
+
+fn main() {
+    let just_x_and_y = JustXAndY { x: 3, y: 8 };
+
+    cartesian_coordinates(just_x_and_y, 19);
+}
