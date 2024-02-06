43d99a8a396b8cc2b86223530486baac6a4840bf
Ore Akintobi <oreakintobi@gmail.com>
2024-02-06 16:24:24 +0100

    my first rust implementation of vectors and iterations

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,4 +10,22 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    // let mut my_numbers = Vec::new();
+    let my_numbers = vec![10, 20, 30, 40];
+
+    // my_numbers.push(10);
+    // my_numbers.push(20);
+    // my_numbers.push(30);
+    // my_numbers.push(40);
+
+    for &num in &my_numbers {
+        if num == 30 {
+            println!("thirty")
+        } else {
+            println!("{:?}", num)
+        }
+    }
+
+    println!("The length of the vector is {:?}", my_numbers.len())
+}
