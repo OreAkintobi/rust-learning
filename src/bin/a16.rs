f53d819f21cb3ec335e7451ba4323de5675d3530
Ore Akintobi <oreakintobi@gmail.com>
2024-02-25 11:25:05 +0100

    my first rust implementation of rust options

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -11,4 +11,33 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
+
+fn main() {
+    // * Print out the details of a student's locker assignment
+    // * Lockers use numbers and are optional for students
+    let student_1 = Student {
+        name: "David".to_owned(),
+        locker: Some(123),
+    };
+    let student_2 = Student {
+        name: "Robert".to_owned(),
+        locker: None,
+    };
+
+    fn print_locker(student: &Student) {
+        match student.locker {
+            Some(locker) => println!("Student: {:?}, Locker: {:?}", student.name, locker),
+            None => println!(
+                "Student: {:?}, Locker: {:?}",
+                student.name, "No locker assigned"
+            ),
+        }
+    }
+
+    print_locker(&student_1);
+    print_locker(&student_2);
+}
