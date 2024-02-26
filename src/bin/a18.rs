b773c6238d2b854faf07098e8834073892aee876
Ore Akintobi <oreakintobi@gmail.com>
2024-02-26 06:40:05 +0100

    my first rust implementation of rust Result & Question Mark Operator

--- src/bin/a18.rs
+++ src/bin/a18.rs
@@ -11,4 +11,36 @@
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
 
-fn main() {}
+/// Struct to store at least the age of a customer
+struct Customer {
+    name: String,
+    age: i32,
+}
+
+/// Determine if a customer can make a purchase
+fn can_make_purchase(customer: &Customer) -> Result<bool, String> {
+    if customer.age >= 21 {
+        Ok(true)
+    } else {
+        Err("Customer is not old enough".to_string())
+    }
+
+    // alternative to the above if-else block
+    // match customer.age {
+    // 	age if age >= 21 => Ok(true),
+    // 	_ => Err("Customer is not old enough".to_string())
+    // }
+}
+
+fn main() {
+    let customer = Customer {
+        name: "Bubba".to_owned(),
+        age: 20,
+    };
+
+    match can_make_purchase(&customer) {
+        Ok(true) => println!("{:?} can make purchase", customer.name),
+        Ok(false) => println!("{:?} cannot make purchase", customer.name),
+        Err(e) => println!("{:?} cannot make purchase: {}", customer.name, e),
+    }
+}
--- src/bin/a18b.rs
+++ src/bin/a18b.rs
@@ -13,13 +13,93 @@
 // * Ensure that terminated employees cannot access the building
 //   regardless of their position
 //
-// Notes:
+
 // * Use an enum to represent all types of employees
+enum EmployeeType {
+    Maintenance,
+    Marketing,
+    Manager,
+    LineSupervisor,
+    KitchenStaff,
+    AssemblyTechnician,
+}
+
 // * Use a struct to store the employee type and whether they are
 //   still employed
+struct Employee {
+    employee_type: EmployeeType,
+    employed: bool,
+}
+
 // * Use a function that returns a Result to determine if the employee
 //   may enter the building
+fn can_access_building(employee: &Employee) -> Result<bool, String> {
+    match employee {
+        Employee {
+            employed: false, ..
+        } => Err("Employee is not employed".to_owned()),
+        Employee {
+            employee_type: EmployeeType::Maintenance,
+            ..
+        } => Ok(true),
+        Employee {
+            employee_type: EmployeeType::Marketing,
+            ..
+        } => Ok(true),
+        Employee {
+            employee_type: EmployeeType::Manager,
+            ..
+        } => Ok(true),
+        _ => Ok(false),
+    }
+}
+
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
+fn print_access_result(employee: &Employee) -> Result<(), String> {
+    let result = can_access_building(employee)?;
+    println!("Employee may access building: {}", result);
+    Ok(())
+}
 
-fn main() {}
+fn main() {
+    let employee1 = Employee {
+        employee_type: EmployeeType::Maintenance,
+        employed: false,
+    };
+    let employee2 = Employee {
+        employee_type: EmployeeType::Marketing,
+        employed: true,
+    };
+    let employee3 = Employee {
+        employee_type: EmployeeType::LineSupervisor,
+        employed: true,
+    };
+    let employee4 = Employee {
+        employee_type: EmployeeType::Manager,
+        employed: true,
+    };
+    let employee5 = Employee {
+        employee_type: EmployeeType::KitchenStaff,
+        employed: true,
+    };
+    let employee6 = Employee {
+        employee_type: EmployeeType::AssemblyTechnician,
+        employed: true,
+    };
+    let employee7 = Employee {
+        employee_type: EmployeeType::Maintenance,
+        employed: true,
+    };
+
+    let employees = vec![
+        employee1, employee2, employee3, employee4, employee5, employee6, employee7,
+    ];
+
+    for employee in employees {
+        match print_access_result(&employee) {
+            Err(e) => println!("Error: {}", e),
+            _ => (),
+        };
+    }
+}
